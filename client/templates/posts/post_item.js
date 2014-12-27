de344d4e3e0999e415b40720e3c381806517c5de
Nan Wang <nantas@gmail.com>
2014-12-27 12:47:03 +0800

    updated microscope to latency compensate.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,7 +1,11 @@
 Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
-  }
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    },
+    submitTime: function() {
+        var date = this.submitted;
+        return date.getHours() + ":" + date.getMinutes() + " " + date.getFullYear() + "-" + (date.getMonth() + 1).toString() + "-" + date.getDate();
+    }
 });
